e264d5f8c2296ac1b1b452a13702916ec2a2a920
cnmicha <10116544+cnmicha@users.noreply.github.com>
2021-09-10 18:54:47 +0200

    Use x-real-ip instead of x-forwarded-for header, removed debug output

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ assigning the Cloudflare DNS servers to it.
 Please note that using a NGINX or Apache reverse proxy for HTTPS is recommended as this application only provides an
 HTTP server.
 
-For client IP detection, the `x-forwarded-for` HTTP header is used (if present). Otherwise, there is a fallback to the
+For client IP detection, the `x-real-ip` HTTP header is used (if present). Otherwise, there is a fallback to the
 layer 3 IP address.
 
 ## Installation with Docker (recommended for production)
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,8 +8,6 @@ export class AppController {
 
   @Put('record/:id/updateIpAddress')
   async putRecordIpAddress(@Param('id') id: string, @Req() req) {
-    console.log('Request headers: ' + JSON.stringify(req.headers));
-
     const record = await this.prisma.cloudflareDnsRecord.findUnique({
       where: { id: Number(id) },
       include: { cloudflareDnsZone: true },
@@ -25,7 +23,7 @@ export class AppController {
       throw new ForbiddenException();
     }
 
-    const clientIpAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const clientIpAddress = req.headers['x-real-ip'] || req.socket.remoteAddress;
 
     console.log('Client IP address: ' + JSON.stringify(clientIpAddress));
 
