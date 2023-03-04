affd0bf5d837bbc222a6571da6f4b8e7fe203681
cnmicha <10116544+cnmicha@users.noreply.github.com>
2023-03-04 20:22:03 +0100

    Fix initializing helmet (required by dependency update)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as helmet from 'helmet';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,4 +9,5 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
+
 bootstrap();
